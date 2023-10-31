9028b32ad17fa0e90ed4df3bc31eef298835dd4a
Windokk <68743762+Windokk@users.noreply.github.com>
2023-10-31 11:46:47 +0100

    Changed engine name to "Crystal Engine""

--- srcs/Libs/stb.cpp
+++ srcs/Libs/stb.cpp
@@ -1,4 +1,4 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include"../Libraries/include/stb/stb_image.h"
-#include"../Libraries/include/stb/stb_image_write.h"
+#include"../../Libraries/include/stb/stb_image.h"
+#include"../../Libraries/include/stb/stb_image_write.h"
