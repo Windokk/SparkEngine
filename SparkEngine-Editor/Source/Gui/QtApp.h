5982563f52247a12b6dbfcd53758e80613e7a899
Windokk <eliptik.windokk@gmail.com>
2024-01-18 21:25:23 +0100

    Added custom window title bar support

--- SparkEngine-Editor/Source/Gui/QtApp.h
+++ SparkEngine-Editor/Source/Gui/QtApp.h
@@ -1,16 +1,76 @@
 #pragma once
 
-#include <QtWidgets/QMainWindow>
-#include "ui_QtApp.h"
 
-class QtWidgetsApplication1 : public QMainWindow
+#ifndef UI_SPARK_ENGINE_APP
+#define UI_SPARK_ENGINE_APP
+
+#include <QtCore/QVariant>
+#include <QtGui/QIcon>
+#include <QtGui/QMouseEvent>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSizeGrip>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+
+class QtTitleFrame : public QFrame {
+    Q_OBJECT
+
+
+public:
+    QMainWindow* app;
+    QtTitleFrame(QWidget* parent);
+    ~QtTitleFrame();
+    void SetApp(QMainWindow* _app);
+private:
+
+    void mousePressEvent(QMouseEvent* event);
+    void mouseMoveEvent(QMouseEvent* event);
+    int m_nMouseClick_X_Coordinate;
+    int m_nMouseClick_Y_Coordinate;
+};
+
+
+class QtApp : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    QtWidgetsApplication1(QWidget* parent = nullptr);
-    ~QtWidgetsApplication1();
+    QtApp(QWidget* parent = nullptr);
+    ~QtApp();
 
-private:
-    Ui::QtWidgetsApplication1Class ui;
+    void retranslateUi();
+    void setupUi();
+
+    QWidget* centralwidget;
+    QVBoxLayout* drop_shadow_layout;
+    QFrame* background;
+    QVBoxLayout* verticalLayout;
+    QtTitleFrame* title;
+    QHBoxLayout* horizontalLayout;
+    QFrame* frame_title;
+    QVBoxLayout* verticalLayout_2;
+    QFrame* frame_btns;
+    QHBoxLayout* horizontalLayout_3;
+    QPushButton* btn_minimize;
+    QPushButton* btn_maximize;
+    QPushButton* btn_close;
+    QFrame* content;
+    QVBoxLayout* verticalLayout_13;
+    QFrame* bottomBar;
+    QHBoxLayout* horizontalLayout_7;
+    QFrame* bottomSpacer;
+    QFrame* resizer;
+    QLabel* resizer_icon;
+    QSizeGrip* resizer_grip;
+
+public Q_SLOTS:
+    bool maximizeSlot();
 };
+
+#endif
